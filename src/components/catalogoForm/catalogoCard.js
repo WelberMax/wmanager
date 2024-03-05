c9e32ec3011c65de4387423afefa4f385a1daed9
WelberMax <welbermax@gmail.com>
2024-03-05 14:40:23 +0100

    update css

--- src/components/catalogoForm/catalogoCard.js
+++ src/components/catalogoForm/catalogoCard.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import styles from './catalogoCard.module.css';
+import { FaStar } from "react-icons/fa";
 
 
 const CatalogoCard = ({title, genres, plot, year, poster, fullplot}) => {
   
   return (
     <div  className={styles.catalogo_card}>
+       <div className={styles.textoCentro}>
+            <div>
+              <h1><FaStar color='#ffc107' /><FaStar color='#ffc107' /><FaStar color='#ffc107' /><FaStar color='#ffc107' /><FaStar color='#ffc107' /></h1>
+              <h1>{title}</h1>
+            </div>
+      </div>
       <div>
         <img src={poster} alt={title}/>
       </div>   
-      <div className={styles.textoCentro}>
-            <div><h1>{title}</h1></div>
-      </div>
-        
-           
-        
-        
-        
-        
-                   
-        
-        
-
+     
         <div className={styles.catalogo_info}>                  
           <h4 className={styles.genero}><span>Genero: </span> {genres}</h4>
           <h4><span>Ano: </span> {year}</h4>
--- src/components/pages/Testepage.js
+++ src/components/pages/Testepage.js
@@ -3,11 +3,32 @@
 
 function Testepage() {
 
+    const anot = [{
+        id: 1,
+        title: 'anotacao 1',
+        descricao: 'descricao da anotação 1',
+        year: '2023'
+        
+    },
+    {
+        id: 2,
+        title: 'anotação 2',
+        descricao: 'descricao da anotação 2',
+        year: '2024'
+
+    }
+]
+
     
     
     return (
-        <div>
+        <div className="container">
             <h1>Teste de requisiçoes</h1>
+            <ul className="list-group">
+                {anot.map((anot) => (
+                    <li key={anot.id} className="list-group-item">{anot.id} - {anot.title}</li>
+                ))}
+            </ul>
             
            
         </div>
